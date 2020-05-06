198eb935e1eb8aeb1d7535c54170b6527436e8cb
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-05-06 18:53:36 +0200

    Dialog and BottomSheetDialog injection removing SEEDViewModel.kt

--- submob/src/main/java/com/github/mustafaozhan/basemob/bottomsheet/BaseBottomSheetDialogFragment.kt
+++ submob/src/main/java/com/github/mustafaozhan/basemob/bottomsheet/BaseBottomSheetDialogFragment.kt
@@ -5,6 +5,7 @@
 package com.github.mustafaozhan.basemob.bottomsheet
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.DialogFragment
@@ -12,15 +13,20 @@ import androidx.navigation.fragment.findNavController
 import com.github.mustafaozhan.basemob.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import dagger.android.support.AndroidSupportInjection
 
 abstract class BaseBottomSheetDialogFragment : AppCompatDialogFragment() {
 
     private lateinit var bottomSheetDialog: BottomSheetDialog
 
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(DialogFragment.STYLE_NO_FRAME, R.style.BottomSheetDialogStyle)
-        isCancelable = false
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
--- submob/src/main/java/com/github/mustafaozhan/basemob/dialog/BaseDialogFragment.kt
+++ submob/src/main/java/com/github/mustafaozhan/basemob/dialog/BaseDialogFragment.kt
@@ -4,6 +4,13 @@
 
 package com.github.mustafaozhan.basemob.dialog
 
+import android.content.Context
 import androidx.fragment.app.DialogFragment
+import dagger.android.support.AndroidSupportInjection
 
-abstract class BaseDialogFragment : DialogFragment()
+abstract class BaseDialogFragment : DialogFragment() {
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+}
