08c04030fdc79ab0805472cd90ec08de3df1fb0f
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-06-02 23:36:15 +0200

    getNavigationResult and setNavigationResult added

--- submob/src/main/java/com/github/mustafaozhan/basemob/util/Extension.kt
+++ submob/src/main/java/com/github/mustafaozhan/basemob/util/Extension.kt
@@ -5,9 +5,11 @@
 
 package com.github.mustafaozhan.basemob.util
 
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.github.mustafaozhan.basemob.model.SingleLiveData
 
 fun <T> LiveData<T>.reObserve(owner: LifecycleOwner, observer: Observer<T>) {
@@ -18,6 +20,13 @@ fun <T> LiveData<T>.reObserve(owner: LifecycleOwner, observer: Observer<T>) {
 fun <T> SingleLiveData<T>.reObserveSingle(owner: LifecycleOwner, observer: Observer<T>) {
     removeObserver(observer)
     observe(owner, observer)
+}
+
+fun <T> Fragment.getNavigationResult(key: String) =
+    findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<T>(key)
+
+fun <T> Fragment.setNavigationResult(result: T, key: String) {
+    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, result)
 }
 
 @Suppress("unused")
