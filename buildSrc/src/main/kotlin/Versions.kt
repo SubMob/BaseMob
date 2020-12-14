ce3d6c7ed596637d557ba7419b01d6c0b641e3ad
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-12-14 23:58:18 +0300

    reverting back to stable android plugin version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@
 
 object Versions {
     const val kotlin = "1.4.21"
-    const val androidPlugin = "7.0.0-alpha02"
+    const val androidPlugin = "4.0.2"
     const val androidMaterial = "1.3.0-alpha04"
     const val navigation = "2.3.2"
 }
