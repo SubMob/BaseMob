d7f471ead51e449d070381f73fcfe8d111dde191
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-12-20 20:56:33 +0300

    gradle clean up

--- submob/build.gradle.kts
+++ submob/build.gradle.kts
@@ -20,11 +20,6 @@ android {
             versionName = getVersionName(project)
         }
 
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
         buildFeatures {
             viewBinding = true
             dataBinding = true
