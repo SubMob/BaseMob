0b791524c615d128a4556db2bfca5066830fed72
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-01-07 09:59:37 +0100

    android plugin update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
+ * Copyright (c) 2021 Mustafa Ozhan. All rights reserved.
  */
 
 object Versions {
     const val kotlin = "1.4.30-M1"
-    const val androidPlugin = "7.0.0-alpha03"
+    const val androidPlugin = "7.0.0-alpha04"
     const val androidMaterial = "1.3.0-beta01"
     const val navigation = "2.3.2"
 }
