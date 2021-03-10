a780626774583c2e67d3f90235e79c596cbc2916
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-03-10 10:47:38 +0100

    dependency updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@
 
 object Versions {
     const val kotlin = "1.4.31"
-    const val androidPlugin = "7.0.0-alpha08"
+    const val androidPlugin = "7.0.0-alpha09"
     const val androidMaterial = "1.4.0-alpha01"
     const val navigation = "2.3.3"
 }
