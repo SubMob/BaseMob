822c832827b42fe06ea15a4a53865ed68bc793d8
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-07-03 21:03:04 +0200

    Gradle update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,6 +5,6 @@
 object Versions {
     const val kotlin = "1.5.20"
     const val androidPlugin = "7.1.0-alpha02"
-    const val androidMaterial = "1.4.0-rc01"
+    const val androidMaterial = "1.4.0"
     const val navigation = "2.3.5"
 }
