36ea7b9acaab0b90dc247cc6caedf5eb60817b8e
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-09-18 20:45:38 +0200

    android gradle plugin update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@
 
 object Versions {
     const val KOTLIN = "1.5.30"
-    const val ANDROID_GRADLE_PLUGIN = "7.1.0-alpha11"
+    const val ANDROID_GRADLE_PLUGIN = "7.1.0-alpha12"
     const val ANDROID_MATERIAL = "1.4.0"
     const val NAVIGATION = "2.3.5"
 }
