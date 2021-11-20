1f2d614d6fb6fb8601ff0ccf8a338bf26742c3a2
Mustafa Ozhan <mustafa.ozhan@wetter.com>
2021-11-20 22:11:48 +0100

    [Oztechan/CCC#224] Stable Library updates and simplifying buildSrc

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,8 +3,8 @@
  */
 
 object Versions {
-    const val KOTLIN = "1.5.31"
-    const val ANDROID_GRADLE_PLUGIN = "7.2.0-alpha04"
+    const val KOTLIN = "1.6.0"
+    const val ANDROID_GRADLE_PLUGIN = "7.0.3"
     const val ANDROID_MATERIAL = "1.4.0"
     const val NAVIGATION = "2.3.5"
 }
