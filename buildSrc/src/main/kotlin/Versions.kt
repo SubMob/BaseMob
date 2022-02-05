c560a136e7f1e0a4e3622ac7945235f4bb343ccc
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-05 11:42:43 +0100

    [Oztechan/CCC#435] Gradle Plugin Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@
 
 object Versions {
     const val KOTLIN = "1.6.10"
-    const val ANDROID_GRADLE_PLUGIN = "7.0.4"
+    const val ANDROID_GRADLE_PLUGIN = "7.1.1"
     const val ANDROID_MATERIAL = "1.4.0"
     const val NAVIGATION = "2.3.5"
 }
