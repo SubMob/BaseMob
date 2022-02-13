964404dba2864fd025dd5d4da116e33cbbf23f3b
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-13 11:40:58 +0100

    [Oztechan/CCC#446] Update Libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,5 +6,5 @@ object Versions {
     const val KOTLIN = "1.6.10"
     const val ANDROID_GRADLE_PLUGIN = "7.1.1"
     const val ANDROID_MATERIAL = "1.4.0"
-    const val NAVIGATION = "2.3.5"
+    const val NAVIGATION = "2.4.1"
 }
