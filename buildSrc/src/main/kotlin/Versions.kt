2639e0962cca39d8dacce51d51cc1b9aa2a155f3
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-13 22:35:10 +0100

    [Oztechan/CCC#446] revert back android navigation update due to crash

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,6 +5,6 @@
 object Versions {
     const val KOTLIN = "1.6.10"
     const val ANDROID_GRADLE_PLUGIN = "7.1.1"
-    const val ANDROID_MATERIAL = "1.5.0"
+    const val ANDROID_MATERIAL = "1.4.0"
     const val NAVIGATION = "2.3.5"
 }
