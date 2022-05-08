0fcf6928899f2449bf07859e947dce92c172ad78
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-05-08 21:13:33 +0300

    [#4] Update Dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,6 +5,6 @@
 object Versions {
     const val KOTLIN = "1.6.21"
     const val ANDROID_GRADLE_PLUGIN = "7.1.3"
-    const val ANDROID_MATERIAL = "1.6.0"
-    const val NAVIGATION = "2.4.2"
+    const val ANDROID_MATERIAL = "1.4.0"
+    const val NAVIGATION = "2.3.5"
 }
