7898e92949f6ac24f10b13c02e05f110b29a0073
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-06-04 10:34:06 +0200

    [#9] Add DependencyUpdates Task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 /*
  * Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
  */
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id(Dependencies.Plugins.DEPENDENCY_UPDATES) version Versions.DEPENDENCY_UPDATES
+}
 
 buildscript {
     repositories {
@@ -20,4 +25,23 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    gradleReleaseChannel = "current"
+    rejectVersionIf { candidate.version.isNonStable() }
+}
+
+fun String.isNonStable(): Boolean {
+    val stableKeyword = listOf(
+        "RELEASE",
+        "FINAL",
+        "GA"
+    ).any {
+        this.toUpperCase(java.util.Locale.ROOT).contains(it)
+    }
+
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(this)
+    return isStable.not()
 }
