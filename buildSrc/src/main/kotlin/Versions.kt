b79046ae4c572bfaccab948fc70ccc5734d3923f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-11-12 15:05:26 +0300

    [Oztechan/CCC#1298] Dependency Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,9 +3,9 @@
  */
 
 object Versions {
-    const val KOTLIN = "1.7.20"
+    const val KOTLIN = "1.7.21"
     const val ANDROID_GRADLE_PLUGIN = "7.3.1"
     const val ANDROID_MATERIAL = "1.7.0"
     const val NAVIGATION = "2.5.3"
-    const val DEPENDENCY_UPDATES = "0.43.0"
+    const val DEPENDENCY_UPDATES = "0.44.0"
 }
