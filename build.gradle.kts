b6aa728f93d09b4beed3741f671a6ad86d6f0e6f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-01-16 17:38:54 +0200

    [Oztechan/CCC#1717] Remove dependency updates library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,6 @@
 /*
  * Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
  */
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
-plugins {
-    @Suppress("DSL_SCOPE_VIOLATION")
-    alias(libs.plugins.dependencyUpdates)
-}
-
 buildscript {
     repositories {
         google()
@@ -26,23 +19,4 @@ allprojects {
         google()
         mavenCentral()
     }
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    gradleReleaseChannel = "current"
-    rejectVersionIf { candidate.version.isNonStable() }
-}
-
-fun String.isNonStable(): Boolean {
-    val stableKeyword = listOf(
-        "RELEASE",
-        "FINAL",
-        "GA"
-    ).any {
-        this.toUpperCase(java.util.Locale.ROOT).contains(it)
-    }
-
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(this)
-    return isStable.not()
 }
