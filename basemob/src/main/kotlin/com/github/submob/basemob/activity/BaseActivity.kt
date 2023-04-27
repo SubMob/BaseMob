046b99c6fd1fc12291fbb609aec4adc3c7308e8a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-04-27 20:11:49 +0200

    [Oztechan/CCC#2311] Make BaseActivity abstract and move the layout into extenders

--- basemob/src/main/kotlin/com/github/submob/basemob/activity/BaseActivity.kt
+++ basemob/src/main/kotlin/com/github/submob/basemob/activity/BaseActivity.kt
@@ -3,16 +3,13 @@
  */
 package com.github.submob.basemob.activity
 
-import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import com.github.submob.basemob.R
 
 @Suppress("unused")
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
 
-    @IdRes
-    protected var containerId: Int = R.id.content
+    protected abstract var containerId: Int
 
     protected fun getNavigationController() = findNavController(containerId)
 }
