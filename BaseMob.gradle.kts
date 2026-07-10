1e47d29180bb0b2303a113dba4095baf7992eda1
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2026-07-10 19:50:30 +0200

    Migrate publishing to Central Portal (vanniktech + auto-release) (#278)
    * [SubMob/BaseMob#277] Migrate publishing to Central Portal (vanniktech + auto-release)
    
    * [SubMob/BaseMob#277] Migrate publishing to Central Portal (vanniktech + auto-release)
    
    * [SubMob/BaseMob#277] Bump to next major (3.0) to mark the publishing overhaul

--- BaseMob.gradle.kts
+++ BaseMob.gradle.kts
@@ -6,11 +6,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     libs.plugins.apply {
         alias(androidLibrary).apply(false)
+        alias(mavenPublish).apply(false)
     }
 }
 
 allprojects {
     project.dependencyLocking.lockAllConfigurations()
+
+    // Group + POM metadata + Central Portal config come from gradle.properties (read by
+    // com.vanniktech.maven.publish). Only the version is dynamic (git commit count).
+    version = ProjectSettings.getVersionName(project)
 
     repositories {
         google()
